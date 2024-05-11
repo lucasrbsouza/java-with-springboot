379ee898b8368770c3417d16cf0d4d3720586553
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2024-05-11 08:36:24 -0300

    udpate PersonController

--- src/main/java/com/javawithsprinbootlucas/javawithspringboot/controller/PersonController.java
+++ src/main/java/com/javawithsprinbootlucas/javawithspringboot/controller/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
 
